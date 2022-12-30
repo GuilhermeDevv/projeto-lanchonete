0b431195ff0b77c2fa12b99cb13c7b544ca9eb4e
unknown <you@example.comddc3.0cf1@gmail.com>
2022-12-30 15:33:34 -0300

    criação do componete de texto

---
+++ pedido lanchonete/src/components/Texto/TextoPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { TitlePage } from './styled';
+
+export function TextoPage({children}) {
+    return (
+        <TitlePage>{children}</TitlePage>
+    );
+}
+
---
+++ pedido lanchonete/src/components/Texto/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+
+
+export const TitlePage = styled.h1`
+    color: black;
+    font-weight: 700;
+    font-size: 3.5rem;
+    text-align: center;
+    margin-bottom: 3px;
+
+`
