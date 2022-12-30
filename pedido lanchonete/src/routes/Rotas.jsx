e7667758fd0dc96003fbd8da3e84c5686d4675a8
unknown <you@example.comddc3.0cf1@gmail.com>
2022-12-30 15:32:38 -0300

    colocando url na pagina de cadastro

--- pedido lanchonete/src/routes/Rotas.jsx
+++ pedido lanchonete/src/routes/Rotas.jsx
@@ -1,11 +1,14 @@
+import React from 'react';
 import { Route, Routes } from "react-router-dom"
 import { Login } from "../pages/Login/Login"
-import React from 'react';
+import { Cadastro} from "../pages/Cadastro/Cadastro"
+
 
 export function Rotas() {
     return (
         <Routes>
             <Route path="/" element={<Login />} />
+            <Route path="/cadastro" element={<Cadastro />} />
         </Routes>
     );
 }
