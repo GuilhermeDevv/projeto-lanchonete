3c7abfe4b35f74390ebec7b5ff06c06138932836
unknown <you@example.comddc3.0cf1@gmail.com>
2022-12-30 19:21:18 -0300

    adição do metodo post(criar user)

--- pedido lanchonete/src/controllers/users.js
+++ pedido lanchonete/src/controllers/users.js
@@ -1,8 +1,32 @@
-function cadastrarUsuario(req, res) {
+const bcrypt = require("bcryptjs");
 
-    const { nome, email, senha } = req.body
+async function cadastrarUsuario(req, res) {
+    try {
+        // validar input novamente@
+        const { nome, email, senha } = req.body;
+        if (!nome || !email || !senha) {
+            return res.status(400).json({ error: "Dados inválidos" });
+        }
 
-    return res.json({ nome, email, senha })
+        // Criptografia da senha do usuario
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(senha, salt);
+
+        // Criar usuario
+        const novoUsuario = await createUser({ nome, email, senha: hash });
+
+        // Return response
+        return res.json({ nome, email });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: "Falha ao criar usuário" });
+    }
 }
 
+async function createUser(user) {
+    const ModelCriarUser = require("../models/criarUser");
+    const novoUsuario = new ModelCriarUser(user);
+    await novoUsuario.save();
+    return novoUsuario;
+}
 module.exports = { cadastrarUsuario }
