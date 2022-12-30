8dd133cb8cc0bf78eafa41ea67eb1028b6d31350
unknown <you@example.comddc3.0cf1@gmail.com>
2022-12-30 15:31:49 -0300

    adicao da page cadastro

---
+++ pedido lanchonete/src/pages/Cadastro/Cadastro.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { C_cadastro } from "../../components/Cadastro/C_cadastro"
+export function Cadastro() {
+    return (
+        <>
+            <C_cadastro />
+        </>
+    );
+}
+
