40f4a410b06c32d731db985509c9097f8d7acf6c
unknown <you@example.comddc3.0cf1@gmail.com>
2022-12-30 23:37:37 -0300

    crição da requisao de acesso

--- pedido lanchonete/src/controllers/users.js
+++ pedido lanchonete/src/controllers/users.js
@@ -1,6 +1,46 @@
 const bcrypt = require("bcryptjs");
+
 const ModelCriarUser = require("../models/criarUser");
+
+function pegarDados(req, res) {
+    // Extrair o email da query da requisição
+    const { email } = req.query;
+    // Procure por um usuário com o email especificado
+    ModelCriarUser.find({ email }, (err, user) => {
+        // Verifique se houve um erro durante a busca
+        if (err) {
+            // Retorne um erro 500 (Erro interno do servidor) e o erro
+            return res.status(500).json({ error: err });
+        }
+
+        // Se o usuário não foi encontrado, retorne um erro 404 (Não encontrado)
+        if (!user) {
+            return res.status(404).json({ error: 'Usuário não encontrado' });
+        }
+        // Se o usuário foi encontrado, retorne um status 200 (OK) e o usuário
+        return res.status(200).json(user);
+    });
+}
+
+async function verificarAcesso(req, res) {
+    const { email, senha } = req.body;
+    //verifica se o campo está vazio
+    if (!email || !senha) {
+        throw new Error("Email e senha são obrigatórios");
+    }
+
+    const usuario = await ModelCriarUser.findOne({ email });
+    if (usuario) {
+        // Usuário encontrado
+        return res.json({ success: true, message: "Usuário encontrado" });
+    } else {
+        // Usuário não encontrado
+        return res.json({ success: false, message: "Usuário não encontrado" });
+    }
+}
+
 async function cadastrarUsuario(req, res) {
+
     try {
         // Validar input novamente por segurança!
         const { nome, email, senha } = req.body;
@@ -27,11 +67,11 @@ async function cadastrarUsuario(req, res) {
         console.error(error);
         return res.status(500).json({ error: "Falha ao criar usuário" });
     }
+    async function createUser(user) {
+        const novoUsuario = new ModelCriarUser(user);
+        await novoUsuario.save();
+        return novoUsuario;
+    }
 }
 
-async function createUser(user) {
-    const novoUsuario = new ModelCriarUser(user);
-    await novoUsuario.save();
-    return novoUsuario;
-}
-module.exports = { cadastrarUsuario }
+module.exports = { verificarAcesso, cadastrarUsuario, pegarDados }
