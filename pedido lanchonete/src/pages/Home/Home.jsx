992042a5b753f707a879534130d101f9e5ddb903
unknown <you@example.comddc3.0cf1@gmail.com>
2022-12-30 23:42:59 -0300

    criando a pagina home

---
+++ pedido lanchonete/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { C_home } from "../../components/Home/C_home"
+
+export function Home(props) {
+    return (
+        <>
+            <C_home />
+        </>
+    );
+}
+
