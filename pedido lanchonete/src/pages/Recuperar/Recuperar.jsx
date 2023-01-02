a16ae0a13f656932990ff280c64ace13b8904eb0
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-01-02 17:19:54 -0300

    correção

--- pedido lanchonete/src/pages/Recuperar/Recuperar.jsx
+++ pedido lanchonete/src/pages/Recuperar/Recuperar.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { C_recuperar } from '../../components/Recuperar/C_recuperar';
 export function Recuperar() {
     return (
-        <>
-            <C_recuperar />
-        </>
+
+        <C_recuperar />
+
     );
 }
 
