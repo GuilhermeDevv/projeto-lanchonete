a2d4cb765adfe6871c54cb6f7c0b108f03f7a2d0
unknown <you@example.comddc3.0cf1@gmail.com>
2023-01-01 22:57:35 -0300

    criação da pagina de recuperar acesso

---
+++ pedido lanchonete/src/pages/Recuperar/Recuperar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { C_recuperar } from '../../components/Recuperar/C_recuperar';
+export function Recuperar() {
+    return (
+        <>
+            <C_recuperar />
+        </>
+    );
+}
+
