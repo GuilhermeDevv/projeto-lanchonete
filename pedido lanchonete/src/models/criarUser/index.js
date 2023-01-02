5e338ab8a3e68ed6948dc87009d64c07e4f5fc1f
unknown <you@example.comddc3.0cf1@gmail.com>
2023-01-01 23:00:07 -0300

    atualização nos dados do banco de dados

--- pedido lanchonete/src/models/criarUser/index.js
+++ pedido lanchonete/src/models/criarUser/index.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose")
 const schema = new mongoose.Schema({
     nome: String,
     senha: String,
-    email: String
+    email: String,
+    temp: [String],
 })
 
 const ModelCriarUser = mongoose.model("usuario", schema)
