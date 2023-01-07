83cc8db30c826d0845019988145fd9eeca14deea
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-01-07 20:06:16 -0300

    criação do componente do incio

---
+++ pedido lanchonete/src/components/Adm/DadosHome/DadosHome.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { ContainerInfo } from "./style"
+
+export function DadosHome() {
+    return (
+        <ContainerInfo>
+            <h1>teste</h1>
+        </ContainerInfo>
+    );
+}
+
