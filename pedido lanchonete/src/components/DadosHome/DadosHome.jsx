0e107f1ea68f3fee70ba7369fd1a0bd39f9740b3
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-01-08 16:30:22 -0300

    alteração das informações

--- pedido lanchonete/src/components/DadosHome/DadosHome.jsx
+++ pedido lanchonete/src/components/DadosHome/DadosHome.jsx
@@ -4,34 +4,34 @@ import { ContainerInfo, ContentInfo } from "./style"
 export function DadosHome() {
     return (
         <ContainerInfo>
-            
+
             <ContentInfo backgroundColor="#96a5a5">
-                <h1>Em preparação: 0</h1>
+                <h1>Pedidos: 0</h1>
             </ContentInfo>
 
             <ContentInfo backgroundColor="#f07a55">
-                <h1>Prontos: 0</h1>
+                <h1>Funcionarios: 0</h1>
             </ContentInfo>
 
             <ContentInfo backgroundColor="#de8484">
-                <h1>Entregues: 0</h1>
+                <h1>Clientes: 0</h1>
             </ContentInfo>
 
             <ContentInfo backgroundColor="#4f7ab9">
-                <h1>pedidos: 0</h1>
+                <h1>faturamento: 0</h1>
             </ContentInfo>
 
             <ContentInfo backgroundColor="#37bf86">
-                <h1>Rejeitados: 0</h1>
+                <h1>produtos: 0</h1>
             </ContentInfo>
 
             <ContentInfo backgroundColor="#2e3e4f">
-                <h1>Avaliação: 0</h1>
+                <h1>vendas: 0</h1>
             </ContentInfo>
 
-
-
-
+            <ContentInfo backgroundColor="red">
+                <h1>avaliações: 0</h1>
+            </ContentInfo>
         </ContainerInfo >
     );
 }
