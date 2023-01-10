2c84889f495bafa6be7dc5d89bdb986e14be172c
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-01-10 00:43:47 -0300

    enviar dados para o banco

--- pedido lanchonete/src/controllers/funcionario.js
+++ pedido lanchonete/src/controllers/funcionario.js
@@ -1,25 +1,25 @@
 const { axios } = require("axios");
 const modelFuncionario = require("../models/funcionarios")
 
-
 async function cadastrarFuncionario(req, res) {
-    const { nome, genero, cpf, cargo } = await req.body
+    // Destruturação dos dados do corpo da requisição
+    const { nome, genero, cpf, cargo } = req.body;
 
+    // Verifica se os campos obrigatórios estão presentes
     if (!nome || !genero || !cpf || !cargo) {
-        return res.status(404).json({ messeger: "Erro, dados em branco", success: false })
-    }
-    const dados = {
-        nome,
-        genero,
-        cpf,
-        cargo
+        return res.status(404).json({ mensagem: "Erro, dados em branco", sucesso: false });
     }
 
-    
+    // Cria uma nova instância do modelo Funcionario
+    const funcionario = new modelFuncionario({ nome, genero, cpf, cargo });
 
+    // Salva a nova instância do modelo Funcionario no banco de dados
+    await funcionario.save();
 
-    return res.json({ dados })
+    // Retorna uma resposta de sucesso
+    return res.json({ mensagem: "Sucesso, funcionário cadastrado", sucesso: true });
 }
+
 
 
 module.exports = { cadastrarFuncionario }
