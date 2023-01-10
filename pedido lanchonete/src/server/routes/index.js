fd142a7b9b588ad3282b2869d0a34f8f861ca503
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-01-10 00:25:45 -0300

    adição da rota de cadastrar usuarios no server

--- pedido lanchonete/src/server/routes/index.js
+++ pedido lanchonete/src/server/routes/index.js
@@ -1,14 +1,26 @@
 const { Router } = require("express");
 const express = require("express");
+//publico
 const { verificarAcesso, cadastrarUsuario, pegarDados, recuperarConta, verificarUrl } = require("../../controllers/users")
+
+//admin
+const { cadastrarFuncionario } = require("../../controllers/funcionario")
+
+//middleware
 const route = require("express").Router();
 
 
+//publico
 route.get("/user", pegarDados)
 route.post("/criarUsuario", cadastrarUsuario)
 route.post("/inicio", verificarAcesso)
 route.post("/recuperar", recuperarConta)
 route.post("/novaSenha", verificarUrl)
+
+
+
+//admin
+route.post("/cadastrarFuncionario", cadastrarFuncionario)
 
 
 module.exports = route
