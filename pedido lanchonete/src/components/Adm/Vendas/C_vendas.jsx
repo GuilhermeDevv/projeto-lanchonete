81d60e0ed2a1f4d2fa9c05c060274cb0458d8b98
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-01-12 17:17:15 -0300

    criação da aba de vendas

---
+++ pedido lanchonete/src/components/Adm/Vendas/C_vendas.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ContainerVendas } from "./style"
+export function C_vendas() {
+    return (
+        <ContainerVendas>
+
+        </ContainerVendas>
+    );
+}
+
