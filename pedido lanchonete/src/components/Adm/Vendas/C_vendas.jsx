fc63040d19e90c0edd84351ee78f786bcb1f2751
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-01-13 18:57:10 -0300

    estrutura do componente e suas funcionalidades

--- pedido lanchonete/src/components/Adm/Vendas/C_vendas.jsx
+++ pedido lanchonete/src/components/Adm/Vendas/C_vendas.jsx
@@ -1,10 +1,60 @@
-import React from 'react';
-import { ContainerVendas } from "./style"
+import React, { useState, useEffect } from 'react';
+import axios from "axios"
+import { ContainerVendas, ContentVendas, Nav, Table, Tbody, Thead } from './style';
+
 export function C_vendas() {
+    const [allvendas, setAllVendas] = useState([
+        { _id: 1, nome: 'gl', nPedido: "gl@gmail.com", status: 'EM ANDAMENTO', valor: "10$" },
+        { _id: 2, nome: 'buru', nPedido: "buru@gmail.com", status: 'Concluido!', valor: "0$" }]
+    );
+    const [nome, setNome] = useState('')
+
+    const filtroDePedido = allvendas.filter(pedidos =>
+        pedidos.nome.toLowerCase().includes(nome.toLowerCase())
+
+    );
+    // useEffect(() => {
+    //     axios.defaults.baseURL = 'http://192.168.1.10:8080'
+    //     axios.get("/listarPedidos", { headers: "Content-Type: application/json" }).then(({ pedidos }) => { setAllVendas(pedidos) });
+    // }, [nome]);
     return (
         <ContainerVendas>
+            <ContentVendas>
+                <div>
+                    <h1>Vendas</h1>
+                    <input
+                        type="text"
+                        placeholder="Pesquisa por nome..."
+                        value={nome}
+                        onChange={event => setNome(event.target.value)}
 
-        </ContainerVendas>
+                    />
+                </div>
+
+                <Table>
+                    <Thead>
+                        <tr>
+                            <th>Cliente</th>
+                            <th>n° do Pedido</th>
+                            <th>Status</th>
+                            <th>Valor</th>
+                        </tr>
+                    </Thead>
+                    <Tbody>
+                        {
+                            filtroDePedido.map(({ _id, nome, nPedido, status, valor }) => (
+                                < tr key={_id}>
+                                    <td>{nome}</td>
+                                    <td>{nPedido}</td>
+                                    <td>{status}</td>
+                                    <td>{valor}</td>
+                                </tr>))
+                        }
+
+                    </Tbody>
+                </Table>
+            </ContentVendas>
+        </ContainerVendas >
     );
 }
 
